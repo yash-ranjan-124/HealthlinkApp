13b11759fc83307bf07abe29951a2e2b838411e9
yranjan <yranjan@in.imshealth.com>
2018-07-18 12:03:08 +0530

    pantients list

--- public/javascripts/home_coach.js
+++ public/javascripts/home_coach.js
@@ -2,75 +2,75 @@
 
 
 
-var HomeLib = function(){
-    return({
-        init:function(){
+var HomeLib = function () {
+    return ({
+        init: function () {
             var device = commonLib.detectDevice();
             var isAndroid = device == "android" ? true : false;
             var isIOS = device == "ios" ? true : false;
-            this.changeNavBarAsPerDevice(isIOS,isAndroid);
+            this.changeNavBarAsPerDevice(isIOS, isAndroid);
             let apptitle = $$("#appTitle").html();
-            
-            $$("#list-button").on("click",function(){
-                $$("#home-content").css("display","none");
-                $$("#clienlist-content").css("display","block");
-                $$("#appTitle").html("Client List")
+
+            $$("#list-button").on("click", function () {
+                $$("#home-content").css("display", "none");
+                $$("#clienlist-content").css("display", "block");
+                $$("#appTitle").html("Patients List")
             });
-            $$("#client-link").on("click",function(){
-                $$("#home-content").css("display","none");
-                $$("#clienlist-content").css("display","block");
-                $$("#appTitle").html("Client List")
+            $$("#client-link").on("click", function () {
+                $$("#home-content").css("display", "none");
+                $$("#clienlist-content").css("display", "block");
+                $$("#appTitle").html("Patients List")
             });
-            $$("#home-button").on("click",function(){
-                $$("#clienlist-content").css("display","none");
-                $$("#home-content").css("display","block");
+            $$("#home-button").on("click", function () {
+                $$("#clienlist-content").css("display", "none");
+                $$("#home-content").css("display", "block");
                 $$("#appTitle").html(apptitle)
             });
-            $$("#home-link").on("click",function(){
-                $$("#clienlist-content").css("display","none");
-                $$("#home-content").css("display","block");
+            $$("#home-link").on("click", function () {
+                $$("#clienlist-content").css("display", "none");
+                $$("#home-content").css("display", "block");
                 $$("#appTitle").html(apptitle);
             });
-            $$("#logout-link").on("click",function(){
+            $$("#logout-link").on("click", function () {
                 commonLib.deleteCookie('type');
                 commonLib.deleteCookie('acc_tkn');
                 console.log("mc");
-                window.location.href="/"+PROGRAM_NAME+"/login";
+                window.location.href = "/" + PROGRAM_NAME + "/login";
             });
         },
-        changeNavBarAsPerDevice(isIOS,isAndroid){
-            if(isAndroid){
-            // Change class
-            console.log("android");
-            $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
-            // And move Navbar into Page
-            $$('.view .navbar').prependTo('.view .page');
-            $$("#appTitle").css('position',''); 
-            $$("#appTitle").css("left"," ");
-            if($$("#appTitle").hasClass('center')){
+        changeNavBarAsPerDevice(isIOS, isAndroid) {
+            if (isAndroid) {
+                // Change class
+                console.log("android");
+                $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+                // And move Navbar into Page
+                $$('.view .navbar').prependTo('.view .page');
+                $$("#appTitle").css('position', '');
+                $$("#appTitle").css("left", " ");
+                if ($$("#appTitle").hasClass('center')) {
                     $$("#appTitle").removeClass('center');
                     $$("#appTitle").removeClass('sliding');
-                    $$("#appTitle").addClass('title'); 
+                    $$("#appTitle").addClass('title');
                     $$("#appTitle").addClass("sliding");
 
                 }
             }
-            if(isIOS){
+            if (isIOS) {
                 console.log("ios");
-                $$("#appTitle").css('position','absolute'); 
-                $$("#appTitle").css("left"," 5rem");
-                if($$("#appTitle").hasClass('title')){
-                      $$("#appTitle").removeClass('title');
-                      $$("#appTitle").removeClass('sliding');
-                      $$("#appTitle").addClass('center');  
-                      $$("#appTitle").addClass("sliding");
+                $$("#appTitle").css('position', 'absolute');
+                $$("#appTitle").css("left", " 5rem");
+                if ($$("#appTitle").hasClass('title')) {
+                    $$("#appTitle").removeClass('title');
+                    $$("#appTitle").removeClass('sliding');
+                    $$("#appTitle").addClass('center');
+                    $$("#appTitle").addClass("sliding");
                 }
             }
         }
 
 
     });
-  
+
 }();
 
 
